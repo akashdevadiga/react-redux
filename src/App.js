f0541e8d5cff814cf46089ee362147d9b1b6c01a
akash <akashdevadiga>
2021-06-17 19:02:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './container/Header';
+import ProductDetail from './container/ProductDetail';
+import ProductListing from './container/ProductListing';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 page no found</Route>
+      </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
